f36173358373d39883ac96b21d4cf64e9eceda20
Owen Bickford <historicstork@gmail.com>
2020-10-29 22:00:06 -0400

    add rna translations

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -9,5 +9,12 @@ defmodule RnaTranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
+    dna
+    |> Enum.map(&translate_char/1)
   end
+
+  defp translate_char(67), do: 71
+  defp translate_char(71), do: 67
+  defp translate_char(84), do: 65
+  defp translate_char(65), do: 85
 end
